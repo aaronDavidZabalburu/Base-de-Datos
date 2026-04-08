5028c451c0e8e77a845bad369d4f2e3de5d9296e
Aaron <aarondavid.marrero.vargas@zabalburu.org>
2026-04-09 00:38:56 +0200

    Añadidas las actividades pendientes

---
+++ SQL/Ejercicios 6A-6B/ACTIVIDAD 6A.-MEZCLA CONSULTAS.sql
@@ -0,0 +1,67 @@
+/*EXAMEN TIPO DML-QL-Consultas*/
+
+/*1.- Mostrar todos los empleados que son comerciales y han nacido entre 1965 y 1975. Mostrar el nombre y sus ventas. */
+
+/*2.- Mostrar cuantos empleados trabajan en oficinas de Madrid.*/
+
+
+/*3.- Mostrar los nombres de los empleados que no han realizado ningún pedido. */
+
+/*4.- Mostrar el número de oficina, nombre del director, región y ventas de las oficinas ordenadas por su región. 
+Si tienen igual región aparecerán primero las que más ventas tengan. */
+
+/*4B.- Si quiero que aparezcan todas las oficinas independientemente de que tengan director o no ¿Cómo lo haríamos? */
+
+/*5.- Mostrar todos los pedidos que han sido realizados en Julio del año 1997. Mostrad todos los datos de pedidos. */
+
+/*6.- Mostrar de cada pedido, el nombre del cliente, nombre del empleado, fecha del pedido en formato largo
+(p ej: miércoles, 6 de enero de 2018), descripción del producto e importe de sus pedidos. */
+
+/*7.- Mostrar de cada empleado: el correo electrónico que será un campo que se forme por la inicial del nombre 
+seguida del primer apellido y del segundo apellido. Tras esto aparecerá “@zabalburu.org” y su fecha de contrato.
+Por ejemplo para Alfonso Garcia Jimenez su correo será agarciajimenez@zabalburu.org.*/
+
+/*8.- Mostrar el nombre de los empleados, nombre de sus jefes, sus ventas y las de sus jefes.
+En el listado deben aparecer todos los empleados, tanto si tienen, jefe, como si no. */
+
+/*9.- Listar los empleados con una cuota superior a la de su jefe; Solo queremos saber el nombre
+y la cuota del empleado. */
+
+/*10.- Hallar el empleado que hizo el primer pedido en la empresa. Mostrad nombre del empleado,
+fecha de pedido, años que han pasado. */
+
+
+
+/*11.- Por cada fabricante, mostrar cuantos productos tiene, el precio medio de sus productos (redondeados a 2 decimales),
+el precio más barato y el precio más caro. Los campos de precios deben aparecer con dos decimales y el símbolo del euro por detrás.
+*/
+
+/*11B.- De la pregunta anterior solo mostrar aquellos fabricantes cuyo precio más alto sea más del doble de la media del precio
+de sus productos.*/
+
+/*12.- Listar la oficina que tenga un objetivo mayor de las que tienen director. 
+Se deben mostrar los campos de número de oficina, nombre del director, objetivo y 
+un objetivo para el año que viene que será el objetivo incrementado en un 5%. 
+A este nuevo campo lo vamos a denominar Objetivo 2021. */
+
+/*13.- Mostrar por cada cliente su nombre, fecha de sus pedidos, y calcular la fecha en la que ha sido enviado, 
+calculándose esta de la siguiente forma:
+
+Si el pedido es inferior a 5000 se envía en 15 días.
+Si el pedido es superior a 5000 e inferior a 15000 se envía en 10 días.
+Si el pedido es superior a 15000 e inferior a 30000 se envía en 5 días.
+Si el pedido es superior a 30000 se envía al día siguiente.
+
+Tanto la fecha del pedido como la fecha del envío debe ser mostrada con el siguiente formato:
+día de mes de año (por ejemplo: 7 de marzo de 2012).
+*/
+
+
+/*14.- Mostrar por cada producto (identificado por su fabricante y producto) su descripción, precio unitario y
+mostrar un campo más denominado cantidad de ventas que muestre un texto dependiendo de la cantidad total
+que de ese producto ha sido vendido a los clientes en el año 2001, si es inferior a 10 mostraremos “poco vendido”,
+si está entre 10 y 50 mostraremos “NOTABLE”, si es superior a 50 mostraremos “MUY VENDIDO”.
+*/
+
+
+
---
+++ SQL/Ejercicios 6A-6B/ACTIVIDAD 6B.- MEZCLA CONSULTAS.sql
@@ -0,0 +1,86 @@
+/*1A.-  Mostrar todos los pedidos realizados en enero de 2007 
+con un precio superior a 25000€. Mostrar el número de pedido y 
+número del empleado que lo ha realizado, 
+número del cliente que lo ha pedido ,
+fecha del pedido  e importe (correctamente formateado). (1 punto)*/
+
+/*1B.- Mostrar todos los pedidos realizados en enero de 2007 con un precio superior a 25000€.
+Mostrar el número de pedido y nombre del empleado que lo ha realizado,
+nombre del cliente que lo ha pedido, fecha del pedido 
+(formato largo ejemplo: lunes, 10 de junio de 2007) 
+e importe (correctamente formateado). */
+
+
+/*2.- Mostrar el número de oficina, ciudad y director de aquellas oficinas que
+no tienen empleados asignados. */
+
+
+/*3.- Quiero mostrar cuantos productos se han pedido del fabricante aci y del fabricante rei.
+Mostrar fabricante y número de productos pedidos. */
+
+
+/*4.- Mostrar cuantos productos se han pedido de cada fabricante.
+Mostrar fabricante y cantidad de productos. */
+
+
+
+/*5.- Mostrar el nombre de los empleados, nombre de sus jefes, sus ventas y cuotas y las de sus jefes.
+En el listado deben aparecer todos los empleados, tanto si tienen, jefe, como si no. */
+
+
+
+/*6.- Listar la oficina que tenga un objetivo mayor de las que tienen director. 
+Se deben mostrar los campos de número de oficina, nombre del director, 
+objetivo y un objetivo para el año que viene que será el objetivo incrementado en un 5%.
+A este nuevo campo lo vamos a denominar Objetivo 2023. */
+
+
+
+/*7.- Listar las oficinas en donde todos los vendedores tienen ventas que superan al 50% 
+del objetivo de su oficina. Mostrar de cada oficina el número, región y objetivo. */
+
+
+
+/*8.- Estamos haciendo un control de existencias y debemos mostrar
+por cada uno de nuestros productos si los pedidos son urgentes, 
+para dentro de una semana, para dentro de un mes.
+Si el campo existencias es menor de 50 unidades el pedido será urgente (URGENTE).
+Si está entre 51 y 150 será para dentro de una semana (SEMANA) 
+y si es superior a 150 será para dentro de un mes (MES). 
+Esta consulta debe mostrar el fabricante, descripción, 
+existencias y la petición del pedido mostrando URGENTE, SEMANA o MES. */
+
+
+/*9.- Mostrar el precio medio de los productos de cada fabricante que superen los 500 €.
+El precio debe aparecer con dos decimales y el símbolo del euro por detrás.
+Mostrar Fabricante y un campo denominado Precio Medio Productos. */
+
+
+/*10.- Mostrar para cada empleado su nombre, su campo ventas, y 
+calcular el premio que van a conseguir según las ventas conseguidas,
+
+Si las ventas son inferiores a 100000 no se dará premio.
+Si las ventas son inferiores a 200000 el premio será de 100€.
+Si las ventas son inferiores a 300000 el premio será de 200€.
+Si las ventas son inferiores a 400000 el premio será de 300€.
+Si las ventas son superiores a 400000 el premio será de 500€.
+Tanto la fecha del pedido como la fecha del envío debe ser mostrada 
+con el siguiente formato: día de mes de año (por ejemplo: 7-marzo-2012).*/
+
+/*11.- Mostrar de cada empleado su nombre, el nombre de su jefe solo de aquellos empleados
+que su jefe haya sido contratado posteriormente a ellos. 
+Aparecerán en el listado también aquellos empleados que no tienen jefe. */  
+
+/*12.- Mostrar por cada empleado el importe mínimo que ha realizado en un pedido
+y el importe máximo en el año 2007. Mostrar nombre del empleado, importe mínimo e importe máximo. */
+
+/*13.- Mostrar los pedidos que ha realizado Ana Bustamante en Julio del 97.
+Mostrar el número de pedido, fecha del pedido en formato largo 
+(p ej: miércoles, 3 de mayo de 2012), y el importe.
+El importe debe aparecer con dos decimales y el símbolo del € detrás. */
+
+/*14.- Se quiere incrementar el objetivo de las oficinas por regiones,
+a aquellas del norte se les incrementará su objetivo en un 5%,
+a las del este en un 3% a las del oeste en un 2% y al resto en un 7%.
+Mostrar en el listado el número de oficina, región, porcentaje a incrementar,
+el objetivo actual y el objetivo actualizado con el incremento. */
